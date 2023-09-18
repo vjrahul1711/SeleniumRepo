0e9e5c8e5a1629651af4bbe3a1c4fdd6cc3f9a75
vjrahul1711 <singh.vijaypartap@gmail.com>
2023-09-18 17:06:41 +0530

    Added new code in main for proceeding with login

--- src/SeleniumLearning/Main.java
+++ src/SeleniumLearning/Main.java
@@ -1,6 +1,9 @@
 package SeleniumLearning;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+
 
 
 class Main{
@@ -8,10 +11,14 @@ class Main{
     public static void main(String[] args) throws InterruptedException {
 
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://www.rummypassion.in");
+        driver.get("https://www.rummypassion.com");
         driver.manage().window().maximize();
         Thread.sleep(4000);
 
-        driver.quit();
+        driver.findElement(By.id("wzrk-cancel")).click(); // Clicking on the popup button
+        driver.findElement(By.linkText("LOGIN")).click();
+        driver.findElement(By.linkText("Login with password instead")).click();
+
+        //driver.quit();
     }
 }
