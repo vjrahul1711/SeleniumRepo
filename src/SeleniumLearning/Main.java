778d6928d1ee9ec20de6dc7314bade53f88eacef
Vijay Pratap <singh.vijaypartap@gmail.com>
2023-09-20 00:53:30 +0530

    nokia

--- src/SeleniumLearning/Main.java
+++ src/SeleniumLearning/Main.java
@@ -1,8 +1,10 @@
 package SeleniumLearning;
 
+import common.configuration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 
@@ -12,15 +14,15 @@ class Main{
     public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://preprodnew.rummypassion.com/");
+        driver.get(configuration.TEST_URL);
         driver.manage().window().maximize();
         Thread.sleep(4000);
 
         driver.findElement(By.id("wzrk-cancel")).click(); // Clicking on the popup button
         driver.findElement(By.linkText("LOGIN")).click();
         driver.findElement(By.linkText("Login with password instead")).click();
-        driver.findElement(By.id("lusername")).sendKeys("tom14");
-        driver.findElement(By.id("lpassword")).sendKeys("test123");
+        driver.findElement(By.id("lusername")).sendKeys(configuration.USER_NAME);
+        driver.findElement(By.id("lpassword")).sendKeys(configuration.PASSWORD);
         driver.findElement(By.xpath("/html/body/section[1]/div/form[1]/div[3]/div/button")).click();
         //driver.quit();
     }
---
+++ src/common/configuration.java
@@ -0,0 +1,12 @@
+package common;
+
+public class configuration {
+
+    public static String USER_NAME = "tom14";
+    public static String PASSWORD = "test123";
+    public static String TEST_URL = "https://preprodnew.rummypassion.com/";
+
+
+
+
+}
