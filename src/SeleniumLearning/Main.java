b898b7bb0037e07353044c1db6a53a8d0b705d82
Vijay Pratap <singh.vijaypartap@gmail.com>
2023-09-19 08:24:05 +0530

    Completed positive case of login

--- src/SeleniumLearning/Main.java
+++ src/SeleniumLearning/Main.java
@@ -11,14 +11,16 @@ class Main{
     public static void main(String[] args) throws InterruptedException {
 
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://www.rummypassion.com");
+        driver.get("https://preprodnew.rummypassion.com/");
         driver.manage().window().maximize();
         Thread.sleep(4000);
 
         driver.findElement(By.id("wzrk-cancel")).click(); // Clicking on the popup button
         driver.findElement(By.linkText("LOGIN")).click();
         driver.findElement(By.linkText("Login with password instead")).click();
-
+        driver.findElement(By.id("lusername")).sendKeys("tom14");
+        driver.findElement(By.id("lpassword")).sendKeys("test123");
+        driver.findElement(By.xpath("/html/body/section[1]/div/form[1]/div[3]/div/button")).click();
         //driver.quit();
     }
 }
