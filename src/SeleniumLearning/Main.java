59d304a6043f60400542c4d6f666c3073729cb87
vjrahul1711 <singh.vijaypartap@gmail.com>
2023-09-21 18:28:06 +0530

    from office

--- src/SeleniumLearning/Main.java
+++ src/SeleniumLearning/Main.java
@@ -24,6 +24,12 @@ class Main{
         driver.findElement(By.id("lusername")).sendKeys(configuration.USER_NAME);
         driver.findElement(By.id("lpassword")).sendKeys(configuration.PASSWORD);
         driver.findElement(By.xpath("/html/body/section[1]/div/form[1]/div[3]/div/button")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("/html/body/div[3]/header/div/div/div/div[1]/div/div[3]/div/ul/li[6]/a")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div/div/div[1]/ul/li[4]/a")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div/div/div[1]/ul/li[4]/ul/li[3]/a")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div/div/div[2]/div/form/div[8]/span[1]/img")).click();
         //driver.quit();
+
     }
 }
