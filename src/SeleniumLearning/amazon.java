092746eeedd2678b11ce69914494e62f9e811cee
Vijay Pratap <singh.vijaypartap@gmail.com>
2023-10-04 23:58:55 +0530

    amazon code

--- src/SeleniumLearning/amazon.java
+++ src/SeleniumLearning/amazon.java
@@ -1,8 +1,22 @@
 package SeleniumLearning;
 
+import common.configuration;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
 
 public class amazon {
+
+    public static void main (String[]args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(configuration.TEST_URL2);
+        driver.manage().window().maximize();
+        driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+
+
+    }
+
 
 
 
--- src/common/configuration.java
+++ src/common/configuration.java
@@ -5,6 +5,7 @@ public class configuration {
     public static String USER_NAME = "tom8";
     public static String PASSWORD = "test123";
     public static String TEST_URL = "https://preprodnew.rummypassion.com/";
+    public static String TEST_URL2 = "https://amazon.in/";
 
 
 
