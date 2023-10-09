7e4dc2cb84c9366fad7b284c078ff9b55fc3052e
vjrahul1711 <singh.vijaypartap@gmail.com>
2023-10-09 18:23:38 +0530

    more code in amazon file

--- src/SeleniumLearning/amazon.java
+++ src/SeleniumLearning/amazon.java
@@ -4,6 +4,8 @@ import common.configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 public class amazon {
 
@@ -12,7 +14,22 @@ public class amazon {
         WebDriver driver = new ChromeDriver();
         driver.get(configuration.TEST_URL2);
         driver.manage().window().maximize();
+    //Login
         driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+        driver.findElement(By.id("ap_email")).sendKeys(configuration.USER_NAME_AMAZON);
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("ap_password")).sendKeys(configuration.PASSWORD_AMAZON);
+        driver.findElement(By.id("signInSubmit")).click();
+
+        //driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+        Actions hover = new Actions(driver);
+        WebElement signInOptions = driver.findElement(By.id("nav-link-accountList-nav-line-1"));
+        hover.moveToElement(signInOptions).perform();
+        WebElement accountOption = driver.findElement(By.linkText("Sign Out"));
+        hover.moveToElement(accountOption).perform();
+        accountOption.click();
+
+
 
 
     }
--- src/common/configuration.java
+++ src/common/configuration.java
@@ -6,6 +6,8 @@ public class configuration {
     public static String PASSWORD = "test123";
     public static String TEST_URL = "https://preprodnew.rummypassion.com/";
     public static String TEST_URL2 = "https://amazon.in/";
+    public static String USER_NAME_AMAZON = "tgsvjp@live.com";
+    public static String PASSWORD_AMAZON = "Ranarahul1711";
 
 
 
