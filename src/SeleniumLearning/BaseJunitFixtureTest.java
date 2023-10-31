e42f0043f51f3ab63331b161530dc37fcf5b816c
vjrahul1711 <singh.vijaypartap@gmail.com>
2023-10-31 21:46:39 +0530

    adding changes

---
+++ src/SeleniumLearning/BaseJunitFixtureTest.java
@@ -0,0 +1,20 @@
+package SeleniumLearning;
+
+import common.configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class BaseJunitFixtureTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
--- src/SeleniumLearning/loginJunitTestTest.java
+++ src/SeleniumLearning/loginJunitTestTest.java
@@ -1,19 +1,20 @@
 package SeleniumLearning;
 
 import common.configuration;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Keys;
+
 import java.util.Iterator;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-
-class LoginLogout{
-
-    public static void main(String[] args) throws InterruptedException {
-
+class loginJunitTestTest {
+    @Test
+    public void test() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get(configuration.TEST_URL);
         driver.manage().window().maximize();
@@ -22,25 +23,12 @@ class LoginLogout{
         driver.findElement(By.id("wzrk-cancel")).click(); // Clicking on the popup button
         driver.findElement(By.linkText("LOGIN")).click(); //Clicking on the login
         driver.findElement(By.linkText("Login with password instead")).click();
-        driver.findElement(By.cssSelector("input[id=lusername]")).sendKeys(configuration.USER_NAME);
-        driver.findElement(By.cssSelector("input[id=lpassword]")).sendKeys(configuration.PASSWORD);
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
+        driver.findElement(By.id("lusername")).sendKeys(configuration.USER_NAME);
+        driver.findElement(By.id("lpassword")).sendKeys(configuration.PASSWORD);
+        driver.findElement(By.xpath("/html/body/section[1]/div/form[1]/div[3]/div/button")).click();
         Thread.sleep(4000);
-
-        //Below commented code is for navigating to my profile and change avtar
-        /*driver.findElement(By.xpath("/html/body/div[3]/header/div/div/div/div[1]/div/div[3]/div/ul/li[6]/a")).click();
-        //driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div/div/div[1]/ul/li[4]/a")).click();
-        //driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div/div/div[1]/ul/li[4]/ul/li[3]/a")).click();
-        //driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div/div/div[2]/div/form/div[8]/span[1]/img")).click();
-        Thread.sleep(3000);*/
-
         driver.findElement(By.xpath("//*[@id=\"Wrapper\"]/header/div/div/div/div[1]/div/div[4]/div/div/a[2]")).click();
         driver.findElement(By.linkText("OK")).sendKeys(Keys.ENTER);
-
-
-
-        // code for switching between windows
-        //driver.findElement(By.xpath("//*[@id=\"dynamic_common_message\"]/div/div/div[3]/ul/li/div/span[2]/a/button"));
         String mainWindowHandle =driver.getWindowHandle();
         System.out.println(mainWindowHandle);
         Set<String> allWindowHandles = driver.getWindowHandles();
@@ -58,4 +46,5 @@ class LoginLogout{
         driver.quit();
 
     }
+
 }
